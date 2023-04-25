0edcca2cbb8f3ebfdeab9b3cec30d6448709b873
Class01 <Class01@DESKTOP-VLQD395>
2023-04-25 17:59:12 +0900

    gogo

--- jongol/src/jongol/Main.java
+++ jongol/src/jongol/Main.java
@@ -3,7 +3,6 @@ package jongol;
 public class Main {
     public static void main(String[] args) {
    	String result= "Fun programming!" ;
-   			
    			System.out.println(result);
     }
 }
---
+++ jongol/src/jongol2/Main2.java
@@ -0,0 +1,7 @@
+package jongol2;
+//sdfdfs
+public class Main2 {
+  public static void main(String[] args) {
+	  System.out.println("Programming! It's fun.");
+  }
+}
