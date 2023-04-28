2d5a2211ac336fecbe33c56e6ef43d7308841fe4
Class01 <Class01@DESKTOP-VLQD395>
2023-04-28 09:48:36 +0900

    9008

---
+++ jongol/jongol/J0428/J9008.java
@@ -0,0 +1,10 @@
+package J0428;
+
+public class J9008 {
+  public static void main(String[] args) {
+	  
+	  System.out.println(" subject score");
+	  System.out.println("==============");
+	  System.out.printf("  korean    90\n english   100\ncomputer    80");
+  }
+}
