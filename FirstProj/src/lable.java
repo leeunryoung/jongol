4f054b8d43109707420c205f6947425067481652
Class01 <Class01@DESKTOP-VLQD395>
2023-07-25 10:01:57 +0900

    w

--- FirstProj/src/lable.java
+++ FirstProj/src/lable.java
@@ -2,7 +2,7 @@
 public class lable {
   public static void main(String[] args) {
 	  int value =10;
-	  int result= value +10;
+	  int result= value *10;
 	  
 	  System.out.println(value);
 	  System.out.println(result);
